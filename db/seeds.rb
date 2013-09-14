ce0659dd4c0f9a5a981f7300887f4732be41f2f7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-09-14 04:28:14 -0300

    Notas para la instalación

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ Configuracion.create unless Configuracion.any?
 if ENV['password'].present?
   Usuario.create(nombre: 'admin', password: ENV['password'])
 else
-  logger.info 'Si querías crear un admin probá de nuevo con: `rake db:seed password="el password del admin global"`'
+  logger.info 'Si querías crear un admin probá de nuevo con:
+
+             rake db:seed password="el password del admin global"'
 end
 
 # Aparatos de acuerdo a los historical existentes. Se crean con cero y escala
@@ -18,3 +20,7 @@ Historical.pluck(:grd_id).uniq.each do |grd|
     aparato.nombre = "id: #{grd}"
   end
 end
+
+logger.info "Después de configurar cada aparato, corré
+
+             rake dusttrak:mediciones_previas"
